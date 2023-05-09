3f7236428d4913c5878ca72cd4060c008614a7f9
Sawyer75 <106007322+Sawyer75@users.noreply.github.com>
2023-05-09 17:05:45 +0300

    Update index.js

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,43 +2,57 @@ const fs = require('fs');
 const path = require('path');
 
 function copyDir(sourceDir, targetDir) {
- 
-  if (!fs.existsSync(targetDir)) {
-    fs.mkdirSync(targetDir);
-  }
-
-  
-  fs.readdir(sourceDir, { withFileTypes: true }, (err, files) => {
+  // Read the contents of the source directory
+  fs.readdir(sourceDir, (err, files) => {
     if (err) {
-      console.error('Error reading source directory:', err);
+      console.error('Error reading directory:', err);
       return;
     }
 
-    
-    for (const file of files) {
-      const sourcePath = path.join(sourceDir, file.name);
-      const targetPath = path.join(targetDir, file.name);
+    // Copy each file from source to target directory
+    files.forEach(file => {
+      const sourcePath = path.join(sourceDir, file);
+      const targetPath = path.join(targetDir, file);
 
-      
-      if (file.isFile()) {
-        
-        fs.copyFile(sourcePath, targetPath, (err) => {
-          if (err) {
-            console.error('Error copying file:', err);
-          }
-        });
-      } else if (file.isDirectory()) {
-       
-        copyDir(sourcePath, targetPath);
-      }
-    }
+      // Check if the current item is a file or a directory
+      fs.stat(sourcePath, (err, stats) => {
+        if (err) {
+          console.error('Error stating file/directory:', err);
+          return;
+        }
+
+        if (stats.isFile()) {
+          // Copy the file to the target directory
+          fs.copyFile(sourcePath, targetPath, err => {
+            if (err) {
+              console.error('Error copying file:', err);
+            }
+          });
+        } else if (stats.isDirectory()) {
+          // Recursively copy the subdirectory
+          fs.mkdir(targetPath, err => {
+            if (err) {
+              console.error('Error creating directory:', err);
+              return;
+            }
+            copyDir(sourcePath, targetPath);
+          });
+        }
+      });
+    });
   });
 }
 
-const sourceDir = '04-copy-directory/files';
-const targetDir = '04-copy-directory/files-copy';
+const sourceDir = path.join(__dirname, 'files');
+const targetDir = path.join(__dirname, 'files-copy');
 
-copyDir(sourceDir, targetDir);
+fs.mkdir(targetDir, err => {
+  if (err) {
+    console.error('Error creating directory:', err);
+    return;
+  }
+  copyDir(sourceDir, targetDir);
+});
 
 
   
