3fdd59a1d0f9566f5bb5a0f8a55d34981c537665
Sawyer75 <106007322+Sawyer75@users.noreply.github.com>
2023-05-10 20:49:35 +0300

    Update index.js

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,38 +1,29 @@
-const fs = require('fs');
+const fs = require('fs-extra');
 const path = require('path');
 
-const stylesDir = '05-merge-styles/styles';
-const distDir = '05-merge-styles/project-dist';
-const outputFileName = 'bundle.css';
+const stylesFolder = path.join(__dirname, 'styles');
+const distFolder = path.join(__dirname, 'project-dist');
+const outputFile = path.join(distFolder, 'bundle.css');
 
-fs.readdir(stylesDir, { withFileTypes: true }, (err, files) => {
-  if (err) {
-    console.error('Error reading styles directory:', err);
-    return;
-  }
+async function collectStyles() {
+  try {
+    const files = await fs.readdir(stylesFolder);
+    const cssFiles = files.filter((file) => path.extname(file) === '.css');
 
-  const cssFiles = files.filter(file => file.isFile() && path.extname(file.name) === '.css');
+    let collectedStyles = '';
 
-  const cssContents = [];
-
-  cssFiles.forEach(file => {
-    const filePath = path.join(stylesDir, file.name);
-    const fileContent = fs.readFileSync(filePath, 'utf-8');
-    cssContents.push(fileContent);
-  });
-
-  const bundleContent = cssContents.join('\n');
-
-  if (!fs.existsSync(distDir)) {
-    fs.mkdirSync(distDir);
-  }
-
-  const bundlePath = path.join(distDir, outputFileName);
-  fs.writeFile(bundlePath, bundleContent, 'utf-8', (err) => {
-    if (err) {
-      console.error('Error writing bundle.css file:', err);
-    } else {
-      console.log('bundle.css file created successfully.');
+    for (const file of cssFiles) {
+      const filePath = path.join(stylesFolder, file);
+      const fileContent = await fs.readFile(filePath, 'utf8');
+      collectedStyles += fileContent;
     }
-  });
-});
+
+    await fs.ensureDir(distFolder);
+    await fs.writeFile(outputFile, collectedStyles);
+
+    console.log('bundle.css file created');
+  } catch (error) {
+    console.error('Error writing bundle.css file:', error);
+  }
+}
+collectStyles();
